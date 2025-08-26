2b97b0d2a0d3e65698bb424999b49a4b67fc1e04
Mowlee-03 <mowleemm@gmail.com>
2025-08-26 17:16:27 +0530

    middleware addedd

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
 var apiRouter = require('./src/routes/apiRoute');
-var publicRouter = require("./src/routes/publicRoute")
+var publicRouter = require("./src/routes/publicRoute");
+const authMiddleware = require('./src/middleware/authMiddleware');
 var app = express();
 
 // view engine setup
@@ -21,8 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use("/gcam",publicRouter)
-app.use('/api', apiRouter);
+app.use("/gcam/common",publicRouter)
+app.use('/api', authMiddleware,apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -1,8 +1,11 @@
 var express = require("express")
 const { createDevice } = require("../controllers/DeviceController")
+const { login, logout } = require("../controllers/AuthController")
 var router = express.Router()
 
 router.post("/device/create",createDevice)
 
+router.post("/login",login)
+router.post("/logout",logout)
 
 module.exports = router
