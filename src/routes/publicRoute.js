a613361dbc660a108db033f5aa8fd048b4d31342
Mowlee-03 <mowleemm@gmail.com>
2025-09-19 17:03:01 +0530

    some changes

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -3,12 +3,12 @@ const { createDevice } = require("../controllers/DeviceController")
 const { login, logout } = require("../controllers/AuthController")
 const { inituser } = require("../controllers/UserManageController")
 const { uploadGarbage } = require("../utils/Multer")
-const { garbagelog } = require("../controllers/LogController")
+// const { garbagelog } = require("../controllers/LogController")
 
 var router = express.Router()
 
 router.post("/device/create",createDevice)
-router.post("/garbage/log",uploadGarbage.single("image_name"),garbagelog)
+// router.post("/garbage/log",uploadGarbage.single("image_name"),garbagelog)
 
 router.post("/login",login)
 router.post("/logout",logout)
